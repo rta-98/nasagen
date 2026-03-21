7a86ed3793c5280f2b7790bcd33e674c739a85a1
rta-98 <treyjroupp@gmail.com>
2026-03-21 14:25:30 -0600

    WIP: working polyfit script

--- nasagen.py
+++ nasagen.py
@@ -27,44 +27,44 @@ class FitShomate:
         out = out.dropna() 
         return out
 
-#    def fit(self, df: pd.DataFrame) -> Dict[str, float]:
-#        T = df[self.T_col].to_numpy(dtype=float)
-#        Cp = df[self.Cp_col].to_numpy(dtype=float)
-#        # Perform fit
-##        Cp_red = Cp / self.R # y 
-##        sh_coeffs = np.polyfit(T, Cp_red, int(4))
-#        A = np.column_stack([np.ones_like(T), T, T**2, T**3, T**4]) # creates an array of ones with the same shape as T; takes each 1D array and stacks them as columns 
-#        # Wrap coefs into a poly fn 
-##        sh_poly = np.poly1d(sh_coeffs)  
-##        Cp_red_fit = sh_poly(T)  
-##        sh_coeffs_asc = sh_coeffs[::-1] 
-#        Cp_fit_coeffs = np.linalg.lstsq(A, Cp, rcond=None)[0]
-#        result = {f"a{i}": float(c) for i, c in enumerate(Cp_fit_coeffs)} 
-#        return result 
-#
-    def fit(self, df:pd.DataFrame) -> Dict[str, float]: 
-
+    def fit(self, df: pd.DataFrame) -> Dict[str, float]:
         T = df[self.T_col].to_numpy(dtype=float)
         Cp = df[self.Cp_col].to_numpy(dtype=float)
-        Tb = int(1000) # split T 
+        #Perform fit
+        Cp_red = Cp / self.R # y 
+        sh_coeffs = np.polyfit(T, Cp_red, int(4))
+        # A = np.column_stack([np.ones_like(T), T, T**2, T**3, T**4]) # creates an array of ones with the same shape as T; takes each 1D array and stacks them as columns 
+        # Wrap coefs into a poly fn 
+        sh_poly = np.poly1d(sh_coeffs)  
+        Cp_red_fit = sh_poly(T)  
+        sh_coeffs_asc = sh_coeffs[::-1] 
+        #Cp_fit_coeffs = np.linalg.lstsq(A, Cp, rcond=None)[0]
+        result = {f"a{i}": float(c) for i, c in enumerate(sh_coeffs_asc)} 
+        return result 
 
-        down = T <= Tb
-        up = T >= Tb 
-
-        T_low, Cp_low = T[low], Cp[low]
-        T_high, Cp_high = T[high], Cp[high]
-
-        A_low = np.column_stack([np.ones_like(T[T_low]), T[T_low], T[T_low]**2, T[T_low]**3, T[T_low]**4]) # creates an array of ones with the same shape as T; takes each 1D array and stacks them as columns 
-        A_high = np.column_stack([np.ones_like(T[T_high]), T[T_high], T[T_high]**2, T[T_high]**3, T[T_high]**4]) # creates an array of ones with the same shape as T; takes each 1D array and stacks them as columns 
-
-        coef_low = np.linalg.lstsq(A_low, Cp_low, rcond=None)[0]
-        coef_high = np.linalg.lstsq(A_high, Cp_high, rcond=None)[0]
-    
-    def Hpoly(Tx, coef):
-        """Integrate Cp quartic""" 
-        a0, a1, a2, a3, a4 = coef  
-        F_x = a1*Tx + 0.5*a2*Tx**2 + (a3/3.0)*Tx**3 + (0.25)*a4*Tx**4 + 0.2*a5*Tx**5 
-        return f"{F_x:,8e}"
+#    def fit(self, df:pd.DataFrame) -> Dict[str, float]: 
+#
+#        T = df[self.T_col].to_numpy(dtype=float)
+#        Cp = df[self.Cp_col].to_numpy(dtype=float)
+#        Tb = int(1000) # split T 
+#
+#        down = T <= Tb
+#        up = T >= Tb 
+#
+#        T_low, Cp_low = T[low], Cp[low]
+#        T_high, Cp_high = T[high], Cp[high]
+#
+#        A_low = np.column_stack([np.ones_like(T[T_low]), T[T_low], T[T_low]**2, T[T_low]**3, T[T_low]**4]) # creates an array of ones with the same shape as T; takes each 1D array and stacks them as columns 
+#        A_high = np.column_stack([np.ones_like(T[T_high]), T[T_high], T[T_high]**2, T[T_high]**3, T[T_high]**4]) # creates an array of ones with the same shape as T; takes each 1D array and stacks them as columns 
+#
+#        coef_low = np.linalg.lstsq(A_low, Cp_low, rcond=None)[0]
+#        coef_high = np.linalg.lstsq(A_high, Cp_high, rcond=None)[0]
+#    
+#    def Hpoly(Tx, coef):
+#        """Integrate Cp quartic""" 
+#        a0, a1, a2, a3, a4 = coef  
+#        F_x = a1*Tx + 0.5*a2*Tx**2 + (a3/3.0)*Tx**3 + (0.25)*a4*Tx**4 + 0.2*a5*Tx**5 
+#        return f"{F_x:,8e}"
 
     def name(self, path: Path) -> str:
         name = path.stem 
